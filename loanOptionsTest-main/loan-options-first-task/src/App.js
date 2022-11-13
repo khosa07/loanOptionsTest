3bf4ab80e6f7033f87f34c513c6726f47c6d838c
khosa07 <asadullah_khosa@hotmail.com>
2022-11-13 17:26:58 +1100

    Added table as well as functionality to load add and delete data within the table

--- loanOptionsTest-main/loan-options-first-task/src/App.js
+++ loanOptionsTest-main/loan-options-first-task/src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const getContacts = () => {
+    fetch("http://universities.hipolabs.com/search?country=Australia")
+      .then((response) => response.json())
+      .then((contactsJson) => setContacts(contactsJson));
+  };
+
+  const removeLastEntry = () => {
+    contacts.pop();
+    setContacts(contacts);
+    console.log(contacts);
+  };
+
+  const addEntry = () => {
+    contacts.push(contacts[0]);
+    setContacts(contacts);
+    console.log(contacts);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Buttons">
+          <button onClick={getContacts} className="LoadButton">
+            Load
+          </button>
+          <button onClick={removeLastEntry} className="DeleteButton">
+            Delete
+          </button>
+          <button onClick={addEntry} className="AddButton">
+            Add
+          </button>
+        </div>
+        <table className="Table">
+          <tr>
+            <th>Country</th>
+            <th>University Name</th>
+            <th>Domain</th>
+            <th>Webpage</th>
+          </tr>
+          {contacts.map((val, key) => {
+            return (
+              <tr key={key} className="Table-spacing">
+                <td>
+                  {val.country} ({val.alpha_two_code})
+                </td>
+                <td>{val.name}</td>
+                <td>{val.domains}</td>
+                <td>{val.web_pages}</td>
+              </tr>
+            );
+          })}
+        </table>
       </header>
     </div>
   );
